42f6443a9cc1927d0f5f2f51bdbe39e2ceed153c
ricky <rickyklu@live.com>
2019-11-30 15:22:42 -0500

    auth users for edit profile and new post"

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -16,11 +16,16 @@ class ProfilesController extends Controller
 
     public function edit(User $user)
     {
+        // authorize access edit route
+        $this->authorize('update', $user->profile);
         // below is the same syntax as above, but using laravel cleaner syntax
         return view('profiles.edit', compact('user'));
     }
 
-    public function update(\App\User $user) {
+    public function update(\App\User $user)
+    {
+
+        $this->authorize('update', $user->profile);
         $data = request()->validate([
             'title' => 'required',
             'description' => 'required',
@@ -28,7 +33,7 @@ class ProfilesController extends Controller
             'image' => '',
         ]);
 
-        $user->profile->update($data);
+        auth()->$user->profile->update($data);
 
         return redirect("/profile/{$user->id}");
     }
